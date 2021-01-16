4108ab383417a30d952e720f20aee681194f3f94
Joshua Lokken <jrrlokken@gmail.com>
2021-01-16 17:35:32 -0600

    Sorting algo exercises

---
+++ bubble.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  const swap = (arr, index1, index2) => {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+
+  return arr;
+}
+
+module.exports = bubbleSort;
